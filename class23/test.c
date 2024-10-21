19763a0e2f995a4c92cb80223422cff6adb69f4c
handsomeliangzai <247883284@qq.com>
2024-10-21 17:53:34 +0800

    update class23:1

--- class23/test.c
+++ class23/test.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+/*
 int main(void)
 {
 	int score;
@@ -32,5 +32,20 @@ int main(void)
 		puts("E");
 	}
 	exit(0);
+}
+*/
+
+int main(void)
+{
+	int year;
+	scanf("%d",&year);
+	if((year%4==0)&&(year%100!=0)||year%400==0)
+	{
+		printf("%d is leap year.\n",year);
+	}
+	else
+	{	
+		printf("%d is not leap year.\n",year);
+	}
 }
 
