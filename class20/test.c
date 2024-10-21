1d4083e392616dec066e9681dd030449c6e1b9f7
handsomeliangzai <247883284@qq.com>
2024-10-21 11:52:58 +0800

    update class20:2

--- class20/test.c
+++ class20/test.c
@@ -18,10 +18,31 @@ static void area(void)
 	return ;
 }
 
+static void root(void)
+{
+	float a,b,c,p,q;
+	float disc;
+	float x1,x2;
+	printf("Please input:");
+	scanf("%f%f%f",&a,&b,&c);
+	if((b*b - 4*a*c)<0)
+	{
+		fprintf(stderr,"EINVAL\n");
+		exit(1);
+	}
+	disc = b*b - 4*a*c;
+	q = sqrt(disc)/2*a;
+	p = -b/2*a;
+	x1 = p+q;
+	x2 = p-q;
+	printf("x1 = %f\nx2 = %f\n",x1,x2);
+	return ;
+}
 
 int main(void)
 {
-	area();
+//	area();
+	root();	
 	exit(0);
 }
 
