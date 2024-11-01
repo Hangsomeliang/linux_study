e95fbdc67642716aed8aa68266a21ed57ac8cc49
handsomeliangzai <247883284@qq.com>
2024-11-01 17:44:16 +0800

    add class70 class 73

---
+++ class73/sqlist.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "sqlist.h"
+
+sqlist *sqlist_create()
+
+void sqlist *sqlist_create1(sqlist **)
+
+int sqlist_insert(sqlist *,int i,datatype *)
+
+int sqlist_delete(sqlist *,int i)
+
+int sqlist_find(sqlist *,datatype *)
+
+int sqlist_isempty(sqlist *)
+
+int sqlist_setempty(sqlist *)
+
+int sqlist_getnum(sqlist*)
+
+int sqlist_destroy(sqlist *)
+
+int sqlist_union(sqlist *,sqlist *)
